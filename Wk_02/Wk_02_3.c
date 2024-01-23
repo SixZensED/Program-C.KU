c9d75c1211ccb7ba1013f06120bd01b6eab8da78
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-01-23 15:34:12 +0700

    Create Wk_02_3.c

---
+++ Wk_02/Wk_02_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main() {
+int x = 5,y = 6;
+  printf("%d\n",x);
+  printf("%c\n",x);
+  printf("%d %d\n",x,y);
+  printf("%d\n",123);
+  printf("%c\n",123);
+  printf("The total is $ %6.2f\n",12.3);
+  printf("The total is $ %6.3f\n",12.3);
+  getch();
+}
