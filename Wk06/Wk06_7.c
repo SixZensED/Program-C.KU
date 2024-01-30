23b795a1641c6ac1d57ad01b8bc26866e85abbf0
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-01-30 11:29:12 +0700

    Update Wk06_7.c

--- Wk06/Wk06_7.c
+++ Wk06/Wk06_7.c
@@ -5,7 +5,7 @@ main() {
 	float sum = 0,avg = 0,money[2][7];
 	for (i=0;i<2;i++) {
 		for (j = 0;j<7;i++) {
-			printf("enter money for day %d in week %d :",j+1,i+1);
+			printf("enter money for day %d in week %d :",i+1,j+1);
 			scanf("%d",&money[i][j]);
 			sum = sum + money[i][j];
 		}
