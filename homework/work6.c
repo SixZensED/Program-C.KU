82d70a04fa2ded70ebaad8d9c5a122716f7e8dfc
SixZens <106856251+SixZensED@users.noreply.github.com>
2024-01-30 13:42:56 +0700

    Update work6.c

--- homework/work6.c
+++ homework/work6.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
 
+main() {
+	char input[1000];
+	int list[26] = {0};
+	
+	printf("enter your character :");
+	gets(input);
+	
+	for (int i = 0; input[i] != '\0';i++) {
+		char getchar = input[i];
+		getchar = tolower(getchar);
+		list[getchar - 'a']++;
+	}
+	for (int i = 0;i < 26;i++) {
+		if (list[i] > 0) {
+			printf("%c %d\n",'a' + i,list[i]);
+		}
+	}
+}
